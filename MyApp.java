bf2a74e89e0743aeffca8786df8ed7be9bb28750
nemus993 <ndecoy@gmail.com>
2015-04-23 17:14:59 +0200

    MyApp.java

---
+++ MyApp.java
@@ -0,0 +1,18 @@
+package gui;
+
+public class MyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MainFrame mf= new MainFrame();
+		mf.setVisible(true);
+		mf.setDefaultCloseOperation(MainFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+
+}
